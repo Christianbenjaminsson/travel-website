fd028c0966906d7ecbd9dcf8918e71b530acb970
Karin <55209058+kodkarin@users.noreply.github.com>
2021-02-03 14:12:23 +0100

    Added functionality to change language on web page

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 $(document).ready(() => {
+
+    $('.en').hide();
+    $('.sv').show();
+
+    $('#lang-switch').change(function() {
+        let lang = $(this).val();
+        $('.en').hide();
+        $('.sv').hide();
+        if (lang === 'en') {
+            $('.en').show();
+        } else {
+            $('.sv').show();
+        }
+    });
+
     $('.main-form').hide();
     $('.destination-input').on('click', () => {
         $('.main-form').toggle();
         $('#name').focus();
     });
+
+    
 })
+
+
